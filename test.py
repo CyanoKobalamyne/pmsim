b1b5ab4f1bef88d96dc282b9fad0a7e3e7b5d952
Áron Ricardo Perez-Lopez <perez.aron@gmail.com>
2019-05-15 14:52:42 -0400

    Add more simple tests

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ import puppetmaster as pm
 
 
 class TestSimple(TestCase):
-    def _validate_transactions(self, expected_time, transactions):
+    def _validate_transactions(self, expected_time, transactions, n_cores=1):
         sched = pm.Scheduler()
-        m = pm.Machine(1, sched)
+        m = pm.Machine(n_cores, sched)
         result_time = m.run(transactions)
         self.assertEqual(expected_time, result_time)
 
@@ -20,6 +20,36 @@ class TestSimple(TestCase):
         """Single transaction with non-empty read and write sets."""
         tr = pm.Transaction({object(), object()}, {object()}, 77)
         self._validate_transactions(tr.time, {tr})
+
+    def test_03(self):
+        """Two independent transactions running serially."""
+        tr1 = pm.Transaction({object()}, {object()}, 12)
+        tr2 = pm.Transaction({object()}, {object()}, 23)
+        expected = tr1.time + tr2.time
+        self._validate_transactions(expected, {tr1, tr2})
+
+    def test_04(self):
+        """Two independent transactions running concurrently."""
+        tr1 = pm.Transaction({object()}, {object()}, 12)
+        tr2 = pm.Transaction({object()}, {object()}, 23)
+        expected = max(tr1.time, tr2.time)
+        self._validate_transactions(expected, {tr1, tr2}, n_cores=2)
+
+    def test_05(self):
+        """Two transactions reading the same object."""
+        obj = object()
+        tr1 = pm.Transaction({obj, object()}, {object()}, 31)
+        tr2 = pm.Transaction({obj, object()}, {object()}, 26)
+        expected = max(tr1.time, tr2.time)
+        self._validate_transactions(expected, {tr1, tr2}, n_cores=2)
+
+    def test_06(self):
+        """Two transactions writing the same object."""
+        obj = object()
+        tr1 = pm.Transaction({object(), object()}, {obj, object()}, 31)
+        tr2 = pm.Transaction({object()}, {obj}, 26)
+        expected = tr1.time + tr2.time
+        self._validate_transactions(expected, {tr1, tr2}, n_cores=2)
 
 
 if __name__ == "__main__":
