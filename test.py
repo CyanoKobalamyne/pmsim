2ad51151f66d4b739359f6768d26c114fa6bfaec
Áron Ricardo Perez-Lopez <perez.aron@gmail.com>
2019-05-15 14:29:33 -0400

    Change transaction time to non-trivial value

--- test.py
+++ test.py
@@ -6,11 +6,12 @@ import puppetmaster as pm
 
 class TestSimple(TestCase):
     def test_01(self):
+        """Single transaction without objects."""
         sched = pm.Scheduler()
         m = pm.Machine(1, sched)
-        tr = pm.Transaction(set(), set(), 1)
+        tr = pm.Transaction(set(), set(), 42)
         time = m.run({tr})
-        self.assertEqual(1, time)
+        self.assertEqual(42, time)
 
 
 if __name__ == "__main__":
