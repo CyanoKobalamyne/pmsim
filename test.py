62187a1c1c7095d4f5908e2818f6a40461f93d7c
Áron Ricardo Perez-Lopez <perez.aron@gmail.com>
2019-05-15 14:37:03 -0400

    Add test, extract common testing code to function

--- test.py
+++ test.py
@@ -5,13 +5,21 @@ import puppetmaster as pm
 
 
 class TestSimple(TestCase):
-    def test_01(self):
-        """Single transaction without objects."""
+    def _validate_transactions(self, expected_time, transactions):
         sched = pm.Scheduler()
         m = pm.Machine(1, sched)
+        result_time = m.run(transactions)
+        self.assertEqual(expected_time, result_time)
+
+    def test_01(self):
+        """Single transaction without objects."""
         tr = pm.Transaction(set(), set(), 42)
-        time = m.run({tr})
-        self.assertEqual(42, time)
+        self._validate_transactions(tr.time, {tr})
+
+    def test_02(self):
+        """Single transaction with non-empty read and write sets."""
+        tr = pm.Transaction({object(), object()}, {object()}, 77)
+        self._validate_transactions(tr.time, {tr})
 
 
 if __name__ == "__main__":
