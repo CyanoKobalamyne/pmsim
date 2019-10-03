addeed59f4b74db4f980c0f19f57cb3ba280c9b5
Áron Ricardo Perez-Lopez <perez.aron@gmail.com>
2019-10-03 00:02:13 -0400

    Change generated distribution to Zipf's Law

--- generate.py
+++ generate.py
@@ -8,11 +8,13 @@ def gen_transactions(max_read_objects, max_write_objects, max_time, memory_size)
     """Yield `Transaction`s with the specified parameters.
 
     A pool of objects with size `memory_size` is created first,
-    and the read and write sets are chosen uniformly from this set.
+    and the read and write sets are chosen from this set in accordance
+    with Zipf's Law.
 
     Arguments:
-        size_range: minimum and maximum size of read and write sets
-        time_range: minimum and maximum transaction time
+        max_read_objects: maximum size of the read sets
+        max_write_objects: maximum size of the write sets
+        max_time: maximum transaction time
         memory_size: total number of objects from which the read and write
                      sets are selected
 
@@ -22,11 +24,14 @@ def gen_transactions(max_read_objects, max_write_objects, max_time, memory_size)
     """
     object_pool = [object() for _ in range(memory_size)]
 
-    def objects(max_size):
-        return random.choices(object_pool, k=random.randint(1, max_size))
+    def objects(*size_range):
+        min_, max_ = size_range
+        zipf_weights = [1 / (i + 1) for i in range(max_ - min_ + 1)]
+        size = random.choices(list(range(min_, max_ + 1)), weights=zipf_weights)[0]
+        return random.choices(object_pool, k=size)
 
     while True:
         yield Transaction(
-            set(objects(max_read_objects)),
-            set(objects(max_write_objects)),
+            set(objects(1, max_read_objects)),
+            set(objects(0, max_write_objects)),
             random.randint(1, max_time))
