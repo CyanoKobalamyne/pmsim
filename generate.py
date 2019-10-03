0322141f81f66b35483ed3726e3f1ab1ccf62cf9
Áron Ricardo Perez-Lopez <perez.aron@gmail.com>
2019-10-02 22:39:55 -0400

    Change arguments to gen_transactions

--- generate.py
+++ generate.py
@@ -4,14 +4,14 @@ import random
 from puppetmaster import Transaction
 
 
-def gen_transactions(size_range, time_range, memory_size):
+def gen_transactions(max_read_objects, max_write_objects, max_time, memory_size):
     """Yield `Transaction`s with the specified parameters.
 
     A pool of objects with size `memory_size` is created first,
     and the read and write sets are chosen uniformly from this set.
 
     Arguments:
-        size_range: minimum and maximum number of read and write sets
+        size_range: minimum and maximum size of read and write sets
         time_range: minimum and maximum transaction time
         memory_size: total number of objects from which the read and write
                      sets are selected
@@ -20,13 +20,13 @@ def gen_transactions(size_range, time_range, memory_size):
         Transaction: a Transaction with a randomly chosen size and time
 
     """
-    object_pool = {object() for _ in range(memory_size)}
+    object_pool = [object() for _ in range(memory_size)]
 
-    def objects():
-        return random.choices(object_pool, k=random.randint(*size_range))
+    def objects(max_size):
+        return random.choices(object_pool, k=random.randint(1, max_size))
 
     while True:
         yield Transaction(
-            set(objects()),
-            set(objects()),
-            random.randint(*time_range))
+            set(objects(max_read_objects)),
+            set(objects(max_write_objects)),
+            random.randint(1, max_time))
