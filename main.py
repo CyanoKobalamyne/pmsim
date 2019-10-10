1e71f108346638e0a79bbb92b3c7eb1692141f1f
Áron Ricardo Perez-Lopez <perez.aron@gmail.com>
2019-10-10 05:02:25 -0400

    Rewrite main script

--- main.py
+++ main.py
@@ -1,52 +1,74 @@
 """Main executable for running puppetmaster."""
+from argparse import ArgumentParser, FileType
+import json
+import os
 import statistics
-from argparse import ArgumentParser
 
 from generate import gen_transactions
 from puppetmaster import Machine, Scheduler
 
 
+SCHEDULING_TIMES = [0, 1, 2, 5, 10, 20]
+MEMORY_SIZES = [0.01, 0.1, 1, 10, 100]
+
+
 def _main():
     parser = ArgumentParser(description="Run puppetmaster on a range of input "
                             "distributions.")
+    parser.add_argument('template', help="transaction template file",
+                        type=FileType('rt'))
     parser.add_argument('n', help="total number of transactions", type=int)
-    parser.add_argument('max_time', help="maximum transaction time", type=int)
-    parser.add_argument('-s', '--step', help="difference in between "
-                        "transaction times of consecutive runs", default=1,
-                        type=int)
-    parser.add_argument('-t', '--time', help="scheduling time", default=0,
-                        type=int)
-    parser.add_argument('-o', '--objects', help="maximum number of objects in "
-                        "a transaction", default=20, type=int)
-    parser.add_argument('-x', '--multiplier', help="multiplier factor between "
-                        "the difference in the maximum size of the read and "
-                        "write sets for a transaction", default=10, type=int)
-    parser.add_argument('-m', '--memory-size', help="total size of memory",
-                        default=1000, type=int)
     parser.add_argument('-c', '--cores', help="number of cores in the machine",
                         default=4, type=int)
+    parser.add_argument('-s', help="parameter of the Zipf's law distribution",
+                        default=1, type=float)
     parser.add_argument('-r', '--repeats', help="number of times the "
                         "experiment with a given set of parameters is re-run",
-                        default=100, type=int)
+                        default=10, type=int)
     args = parser.parse_args()
-    print(f"Arguments: {vars(args)}\n")
 
-    max_write_objects = int(round(args.objects / (args.multiplier + 1)))
-    max_read_objects = int(round(
-        args.objects * args.multiplier / (args.multiplier + 1)))
+    label = "MemSize \\ SchedTime"
+    hcol = f"{{0:<{max(len(label), len(str(max(MEMORY_SIZES) * args.n)))}}}  "
+    col_width = max(len(f"{1:.3f}"), len(str(max(SCHEDULING_TIMES))))
+    cols = "".join(f"{{{i + 1}:{col_width}.3f}}  "
+                   for i in range(len(SCHEDULING_TIMES)))
+    hline = hcol + "".join(f"{{{i + 1}:{col_width}d}}  "
+                           for i in range(len(SCHEDULING_TIMES)))
+    line = hcol + cols
 
-    print("Tr. time   Min      Avg   Max")
-    for time in range(1, args.max_time + 1, args.step):
-        results = []
-        for i in range(args.repeats):
-            tr_gen = gen_transactions(max_read_objects, max_write_objects,
-                                      time, args.memory_size)
-            transactions = [next(tr_gen) for _ in range(args.n)]
-            machine = Machine(n_cores=args.cores, scheduler=Scheduler(),
-                              scheduling_time=args.time)
-            results.append(machine.run(transactions))
-        print(f"{time:<8d}  {min(results):4d}  {statistics.mean(results):7.2f}"
-              f"  {max(results):4d}")
+    print(f"Average throughput for run with {args.n} transactions and "
+          f"{os.path.basename(args.template.name)} template "
+          f"on {args.cores} cores and s={args.s:.2f}")
+    print("----------")
+    print(hline.format(label, *SCHEDULING_TIMES))
+
+    tr_types = json.load(args.template)
+    total_weight = sum(tr["weight"] for tr in tr_types.values())
+    for tr in tr_types.values():
+        tr["N"] = args.n * tr["weight"] / total_weight
+    serial_runtime = sum(tr["time"] * tr["N"] for tr in tr_types.values())
+    parallel_runtime = serial_runtime / args.cores
+
+    for multiplier in MEMORY_SIZES:
+        mem_size = int(round(multiplier * args.n))
+        avg_throughputs = []
+        for sched_time in SCHEDULING_TIMES:
+            results = []
+            for i in range(args.repeats):
+                transactions = []
+                weight_sum = sum(tr["weight"] for tr in tr_types.values())
+                for name, prop in tr_types.items():
+                    tr_gen = gen_transactions(mem_size, prop["reads"],
+                                              prop["writes"], prop["time"],
+                                              args.s)
+                    N = int(round(args.n * prop["weight"] / weight_sum))
+                    transactions.extend(next(tr_gen) for _ in range(N))
+                machine = Machine(n_cores=args.cores,
+                                  scheduler=Scheduler(),
+                                  scheduling_time=sched_time)
+                results.append(machine.run(transactions))
+            avg_throughputs.append(parallel_runtime / statistics.mean(results))
+        print(line.format(mem_size, *avg_throughputs))
 
 
 if __name__ == "__main__":
