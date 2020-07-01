9051b7e35d34855bc738cc1c5ac5cac7c789c883
Áron Ricardo Perez-Lopez <arpl@mit.edu>
2020-07-01 18:53:26 -0400

    Move nested class to top level

--- generators.py
+++ generators.py
@@ -58,41 +58,42 @@ class RandomFactory(TransactionFactory):
         self.gens += 1
         addr_end = self.tr_count * self.gens
         addresses = SequenceView(self.addresses)[addr_start:addr_end]
-        return self.TrGenerator(self.tr_types, addresses)
-
-    class TrGenerator(Iterator[Transaction]):
-        """Yields new transactions based on configuration and available addresses."""
-
-        def __init__(
-            self, tr_types: Iterable[Mapping[str, int]], addresses: Sequence[int]
-        ) -> None:
-            """Create new TrGenerator."""
-            self.addresses = addresses
-            self.tr_data: List[Mapping[str, int]] = []
-            for type_ in tr_types:
-                self.tr_data.extend(type_ for i in range(type_["N"]))
-            random.shuffle(self.tr_data)
-            self.index = 0
-            self.address_index = 0
-
-        def __next__(self) -> Transaction:
-            """Return next transaction with the given configuration."""
-            if self.index == len(self.tr_data):
-                raise StopIteration
-            tr_conf = self.tr_data[self.index]
-            self.index += 1
-            read_start = self.address_index
-            self.address_index += tr_conf["reads"]
-            read_end = write_start = self.address_index
-            self.address_index += tr_conf["writes"]
-            write_end = self.address_index
-            if self.address_index > len(self.addresses):
-                raise RuntimeError("not enough addresses available")
-            read_set = self.addresses[read_start:read_end]
-            write_set = self.addresses[write_start:write_end]
-            return Transaction(read_set, write_set, tr_conf["time"])
+        return TransactionGenerator(self.tr_types, addresses)
 
     @property
     def total_time(self) -> int:
         """See TransactionGenerator.total_time."""
         return self.total_tr_time
+
+
+class TransactionGenerator(Iterator[Transaction]):
+    """Yields new transactions based on configuration and available addresses."""
+
+    def __init__(
+        self, tr_types: Iterable[Mapping[str, int]], addresses: Sequence[int]
+    ) -> None:
+        """Create new TransactionGenerator."""
+        self.addresses = addresses
+        self.tr_data: List[Mapping[str, int]] = []
+        for type_ in tr_types:
+            self.tr_data.extend(type_ for i in range(type_["N"]))
+        random.shuffle(self.tr_data)
+        self.index = 0
+        self.address_index = 0
+
+    def __next__(self) -> Transaction:
+        """Return next transaction with the given configuration."""
+        if self.index == len(self.tr_data):
+            raise StopIteration
+        tr_conf = self.tr_data[self.index]
+        self.index += 1
+        read_start = self.address_index
+        self.address_index += tr_conf["reads"]
+        read_end = write_start = self.address_index
+        self.address_index += tr_conf["writes"]
+        write_end = self.address_index
+        if self.address_index > len(self.addresses):
+            raise RuntimeError("not enough addresses available")
+        read_set = self.addresses[read_start:read_end]
+        write_set = self.addresses[write_start:write_end]
+        return Transaction(read_set, write_set, tr_conf["time"])
