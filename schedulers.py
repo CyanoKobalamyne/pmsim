ba8be3eafe1f8437f0f728f3caad00234e71e6f9
Áron Ricardo Perez-Lopez <arpl@mit.edu>
2020-07-03 15:14:27 -0400

    Add tournament scheduler

--- schedulers.py
+++ schedulers.py
@@ -1,5 +1,6 @@
 """Scheduler implementations."""
 
+import itertools
 from typing import Iterable
 
 from model import TransactionScheduler
@@ -35,6 +36,53 @@ class ConstantTimeScheduler(TransactionScheduler):
                 candidates.add(tr)
         self._clock += self.scheduling_time
         return candidates
+
+    @property
+    def clock(self) -> int:
+        """See TimedComponent.clock."""
+        return self._clock
+
+    @clock.setter
+    def clock(self, value: int) -> None:
+        """See TimedComponent.set_clock."""
+        self._clock = max(self._clock, value)
+
+
+class TournamentScheduler(TransactionScheduler):
+    """Implementation of a "tournament" scheduler."""
+
+    def __init__(self, cycles_per_merge: int, is_pipelined=False):
+        """Initialize a new scheduler.
+
+        Arguments:
+            cycles_per_merge: time it takes to perform one "merge" operation in hardware
+            is_pipelined: whether scheduling time depends on the number of merge steps
+        """
+        self.cycles_per_merge = cycles_per_merge
+        self.is_pipelined = is_pipelined
+        self._clock = 0
+
+    def run(
+        self, pending: Iterable[Transaction], ongoing: Iterable[Transaction]
+    ) -> TransactionSet:
+        """See TransacionScheduler.run.
+
+        Filters out all transactions that conflict with currently running ones, then
+        checks the available transactions pairwise against each other repeatedly, until
+        a single non-conflicting group remains.
+        """
+        # Filter out candidates compatible with ongoing.
+        ongoing = TransactionSet(ongoing)
+        candidates = [TransactionSet([t]) for t in pending if ongoing.compatible(t)]
+        rounds = 0
+        while len(candidates) > 1:
+            for t1, t2 in itertools.zip_longest(candidates[::2], candidates[1::2]):
+                if t2 is not None and t1.compatible(t2):
+                    t1.add(t2)
+            candidates = candidates[::2]
+            rounds += 1
+        self._clock += self.cycles_per_merge * (1 if self.is_pipelined else rounds)
+        return candidates[0] if candidates else TransactionSet()
 
     @property
     def clock(self) -> int:
